6dd2362981b948f9d6039ef2219637171989a791
levi613 <levibelhamou@gmail.com>
2024-12-27 02:14:35 +0100

    essaie sans csrf

--- src/Security/AppCustomAuthenticator.php
+++ src/Security/AppCustomAuthenticator.php
@@ -34,7 +34,7 @@ class AppCustomAuthenticator extends AbstractLoginFormAuthenticator
             new UserBadge($email),
             new PasswordCredentials($request->getPayload()->getString('password')),
             [
-                new CsrfTokenBadge('authenticate', $request->getPayload()->getString('_csrf_token')),
+                // new CsrfTokenBadge('authenticate', $request->getPayload()->getString('_csrf_token')),
                 new RememberMeBadge(),
             ]
         );
