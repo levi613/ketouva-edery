14c825e3dd47d40213fbe937881df0e0e2ee8398
levi613 <92462555+levi613@users.noreply.github.com>
2025-01-16 00:00:11 +0100

    Update PdfGeneratorService.php

--- src/Services/PdfGeneratorService.php
+++ src/Services/PdfGeneratorService.php
@@ -91,7 +91,7 @@ class PdfGeneratorService
         }
 
         // Après la configuration de la police et avant le MultiCell
-        $optimizer = new TextOptimizer($pdf, $width, $x, $y, $lineHeight, 0.85, 0.95);
+        $optimizer = new TextOptimizer($pdf, $width, $x, $y, $lineHeight, 0.85, 0.9);
         $optimizer->renderText($text);
 
         // $pdf->SetXY($x, $y);
